29cea3b356ef5f9e58591e50aba37fac0a70dc2e
cranoo <75974473+cranoo3@users.noreply.github.com>
2025-05-19 02:34:36 +0900

    Initial Commit

---
+++ PagingSample/PagingSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PagingSampleApp.swift
+//  PagingSample
+//
+//  Created by cranoo on 2025/05/19.
+//
+
+import SwiftUI
+
+@main
+struct PagingSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PagingSampleTests/PagingSampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  PagingSampleTests.swift
+//  PagingSampleTests
+//
+//  Created by cranoo on 2025/05/19.
+//
+
+import Testing
+@testable import PagingSample
+
+struct PagingSampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
